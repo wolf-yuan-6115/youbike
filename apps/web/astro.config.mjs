12b4a5c4efd150cee2dcfec46b62343b2ec77056
wolf-yuan <wolf@wolf-yuan.dev>
2025-07-25 00:07:35 +0800

    pref: make dynamic page server side render only

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: cloudflare({
     imageService: "compile",
   }),
