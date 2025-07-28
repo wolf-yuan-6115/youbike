e02f226dba3997ca48a602df8d8d34dd91b7fa57
wolf-yuan <wolf@wolf-yuan.dev>
2025-07-28 18:23:55 +0800

    fix: grabber shows incorrect month

--- apps/grabber/utils/currentTime.ts
+++ apps/grabber/utils/currentTime.ts
@@ -3,7 +3,7 @@ import { formatInTimeZone } from "date-fns-tz";
 export function logCurrentTime(message?: string) {
   const currentTime = new Date();
   console.log(
-    `${currentTime.getMonth().toString().padStart(2, "0")}/${currentTime.getDate().toString().padStart(2, "0")} ${currentTime.getHours().toString().padStart(2, "0")}:${currentTime.getMinutes().toString().padStart(2, "0")} ${message ?? message}`,
+    `${(currentTime.getMonth() + 1).toString().padStart(2, "0")}/${currentTime.getDate().toString().padStart(2, "0")} ${currentTime.getHours().toString().padStart(2, "0")}:${currentTime.getMinutes().toString().padStart(2, "0")} ${message ?? message}`,
   );
 }
 
