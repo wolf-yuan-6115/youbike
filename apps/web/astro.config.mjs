0555ad63268196189b8085c5ad218efb9380b0b3
wolf-yuan <wolf@wolf-yuan.dev>
2025-07-28 22:57:52 +0800

    feat: per station historical view

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   adapter: cloudflare({
     imageService: "compile",
   }),
