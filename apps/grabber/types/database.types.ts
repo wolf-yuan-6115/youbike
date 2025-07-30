e521eed7e127456844496d2b93cf605ac2cedfc1
wolf-yuan <wolf@wolf-yuan.dev>
2025-07-31 00:38:33 +0800

    chore: update supabase and wrangler

--- apps/grabber/types/database.types.ts
+++ apps/grabber/types/database.types.ts
@@ -4,176 +4,167 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   // Allows to automatically instanciate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
-    PostgrestVersion: "12.2.3 (519615d)";
-  };
+    PostgrestVersion: "12.2.3 (519615d)"
+  }
   public: {
     Tables: {
       current: {
         Row: {
-          bikes: number;
-          fail: number;
-          full: number;
-          slots: number;
-          station_id: number;
-          status: Database["public"]["Enums"]["station_state"];
-          success: number;
-          unavailable: number;
-          update: string;
-        };
+          bikes: number
+          full: number
+          slots: number
+          station_id: number
+          status: Database["public"]["Enums"]["station_state"]
+          success: number
+          unavailable: number
+          update: string
+        }
         Insert: {
-          bikes?: number;
-          fail?: number;
-          full?: number;
-          slots?: number;
-          station_id: number;
-          status?: Database["public"]["Enums"]["station_state"];
-          success?: number;
-          unavailable?: number;
-          update?: string;
-        };
+          bikes?: number
+          full?: number
+          slots?: number
+          station_id: number
+          status?: Database["public"]["Enums"]["station_state"]
+          success?: number
+          unavailable?: number
+          update?: string
+        }
         Update: {
-          bikes?: number;
-          fail?: number;
-          full?: number;
-          slots?: number;
-          station_id?: number;
-          status?: Database["public"]["Enums"]["station_state"];
-          success?: number;
-          unavailable?: number;
-          update?: string;
-        };
+          bikes?: number
+          full?: number
+          slots?: number
+          station_id?: number
+          status?: Database["public"]["Enums"]["station_state"]
+          success?: number
+          unavailable?: number
+          update?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "history_station_id_fkey";
-            columns: ["station_id"];
-            isOneToOne: true;
-            referencedRelation: "stations";
-            referencedColumns: ["id"];
+            foreignKeyName: "history_station_id_fkey"
+            columns: ["station_id"]
+            isOneToOne: true
+            referencedRelation: "stations"
+            referencedColumns: ["id"]
           },
-        ];
-      };
+        ]
+      }
       history: {
         Row: {
-          at: string;
-          available: number;
-          empty: number;
-          id: string;
-          station_id: number;
-        };
+          at: string
+          available: number
+          empty: number
+          id: string
+          station_id: number
+        }
         Insert: {
-          at: string;
-          available: number;
-          empty: number;
-          id?: string;
-          station_id: number;
-        };
+          at: string
+          available: number
+          empty: number
+          id?: string
+          station_id: number
+        }
         Update: {
-          at?: string;
-          available?: number;
-          empty?: number;
-          id?: string;
-          station_id?: number;
-        };
+          at?: string
+          available?: number
+          empty?: number
+          id?: string
+          station_id?: number
+        }
         Relationships: [
           {
-            foreignKeyName: "recently_station_id_fkey";
-            columns: ["station_id"];
-            isOneToOne: false;
-            referencedRelation: "stations";
-            referencedColumns: ["id"];
+            foreignKeyName: "recently_station_id_fkey"
+            columns: ["station_id"]
+            isOneToOne: false
+            referencedRelation: "stations"
+            referencedColumns: ["id"]
           },
-        ];
-      };
+        ]
+      }
       stations: {
         Row: {
-          address: string;
-          address_en: string;
-          enabled: boolean;
-          id: number;
-          lat: number;
-          lng: number;
-          name: string;
-          name_en: string;
-          total: number | null;
-        };
+          address: string
+          address_en: string
+          id: number
+          lat: number
+          lng: number
+          name: string
+          name_en: string
+          total: number
+        }
         Insert: {
-          address: string;
-          address_en: string;
-          enabled: boolean;
-          id: number;
-          lat: number;
-          lng: number;
-          name: string;
-          name_en: string;
-          total?: number | null;
-        };
+          address: string
+          address_en: string
+          id: number
+          lat: number
+          lng: number
+          name: string
+          name_en: string
+          total?: number
+        }
         Update: {
-          address?: string;
-          address_en?: string;
-          enabled?: boolean;
-          id?: number;
-          lat?: number;
-          lng?: number;
-          name?: string;
-          name_en?: string;
-          total?: number | null;
-        };
-        Relationships: [];
-      };
-    };
+          address?: string
+          address_en?: string
+          id?: number
+          lat?: number
+          lng?: number
+          name?: string
+          name_en?: string
+          total?: number
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       get_history_with_limit: {
-        Args: { max_rows?: number };
+        Args: { max_rows?: number }
         Returns: {
-          id: string;
-          station_id: number;
-          available: number;
-          empty: number;
-          at: string;
-        }[];
-      };
-    };
+          id: string
+          station_id: number
+          available: number
+          empty: number
+          at: string
+        }[]
+      }
+    }
     Enums: {
-      station_state: "FULL" | "EMPTY" | "NORMAL";
-    };
+      station_state: "FULL" | "EMPTY" | "NORMAL"
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
 
-type DefaultSchema = DatabaseWithoutInternals[Extract<
-  keyof Database,
-  "public"
->];
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
       DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
@@ -181,95 +172,95 @@ export type Tables<
         DefaultSchema["Views"])
     ? (DefaultSchema["Tables"] &
         DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R;
+        Row: infer R
       }
       ? R
       : never
-    : never;
+    : never
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I;
+        Insert: infer I
       }
       ? I
       : never
-    : never;
+    : never
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U;
+        Update: infer U
       }
       ? U
       : never
-    : never;
+    : never
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
 > = DefaultSchemaEnumNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never;
+    : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never;
+    : never
 
 export const Constants = {
   public: {
@@ -277,4 +268,4 @@ export const Constants = {
       station_state: ["FULL", "EMPTY", "NORMAL"],
     },
   },
-} as const;
+} as const
