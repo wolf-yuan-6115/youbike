58d30da059369b3e0e9961d15eca5ccd17212370
wolf-yuan <wolf@wolf-yuan.dev>
2025-08-01 23:49:26 +0800

    feat: update markdown and updated sql commands

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 # Where is my YouBike?
 
-A web page that can view historical data of YouBike availability.
-
-Powered by Bun, Supabase and TypeScript. Website part uses Astro, the blazing fast web framework.
-
+> aka *Cha Bike*
+> 
+> `Cha`, referring to Chinese word `查` (chá). Means to search and query.
+> 
 > YouBike is a popular public bike rental service that has stations everywhere.
+
+A web app/service that can view historical data of YouBike availability.
 
 ## Why?
 
-Because they sometimes (currently elevating to often) leave the station near my school without a bike, forcing me to walk home or take the bus instead.
+Because lately—*and by lately I mean way too often*—the station near my school is completely out of bikes.
+Not even one sad little leftover with a flat tire. Just empty docks mocking me in the heat.
+
+So instead of a quick ride home, I’m stuck walking or paying for a crowded bus ride.
+This project is my way of keeping tabs on the situation—and maybe throwing some passive-aggressive shade when they fail to stock bikes during peak hours.
+
+
+## Tech stacks
+
+- Data grabber
+  - Runs on [**Cloudflare Workers**](https://workers.cloudflare.com/) using [cron trigger](https://developers.cloudflare.com/workers/configuration/cron-triggers/) function
+  - Uses TypeScript
+- Frontend
+  - Written in [Astro](https://astro.build), with [i18n routing](https://docs.astro.build/en/guides/internationalization/) for two languages
+  - Runs on [**Cloudflare Workers**](https://workers.cloudflare.com/) using [Static Assets](https://developers.cloudflare.com/workers/static-assets/) feature
+  - Uses **TailwindCSS** for more rapid development (for me)
+  - Completely rendered on demand
+
+Both instance shares same [Supabase](https://developers.cloudflare.com/workers/static-assets/) database, along with [RLS security](https://supabase.com/docs/guides/database/postgres/row-level-security) for some security.
 
 ## The code
 
@@ -20,30 +40,45 @@ This project is using monorepo structure.
 └── apps
     ├── grabber
     │   └── <Contains data grabber source code>
-    └── web
+    └── frontend
         └── <Contains website source code>
 ```
 
 ### Running by yourself
 
 Please clone this repo and run `bun install` in the project root.
+This will install everything you need to try this project.
 
 #### Data grabber
 
 1. Create a project on Supabase
-2. Run the SQL command inside `apps/grabber/database/init.sql`
-3. Open `apps/grabber`, copy `.env.example` to `.env` and fill in the variables
-   > You need to fill in **Service Key** (**API key** in the newer version of Supabase
-4. Start the grabber by running `bun run ./index.ts` inside `apps/grabber`
+2. Run the SQL command inside `apps/grabber/database/init.sql`, inside the Supabase instance you've created.
+3. Open `wrangler.jsonc` in **grabber** folder, change `SUPABASE_URL` in `vars` section to your Supabase instance URL.
+4. Deploy this project to Cloudflare Workers by connecting your own GitHub / GitLab repository.
+5. Once deployed, head over to **Settings** → **Variables and Secrets** section. Add `SUPABASE_SERVICE_KEY` variable with a Supabase Service role key or Secret keys.
+   > Make sure to set the variable type to secret variable so Cloudflare won't override it when a new version deploys
+6. Hit `Deploy` button once you filled all variables.
 
-There is also a Docker compose file at the project root that will deploy data grabber.
+The data grabber should now start grabbing data per minute and insert them into the Supabase database for website to query.
 
-#### Web page
+#### Frontend
 
-> Currently, the website lies on Cloudflare Pages and uses `@astrojs/cloudflare` adapter for running functions on Pages. You can host it locally by replacing the adapter with `@astrojs/node` or other adapter you want to match your need.
+> Currently, the website lies on Cloudflare Workers and uses `@astrojs/cloudflare` adapter for running functions on Pages.
+> 
+> You can host it locally by replacing the adapter with `@astrojs/node` or other adapter you want to match your need.
 
-1. Deploy this repo to Cloudflare pages
+1. Make sure you have [Data grabber](#data-grabber) deployed.
+2. Open `wrangler.jsonc` in **frontend** folder, change `SUPABASE_URL` in `vars` section to your Supabase instance URL.
+3. Deploy this project to Cloudflare Workers by connecting your own GitHub / GitLab repository.
+4. Once deployed, head over to **Settings** → **Variables and Secrets** section. Add `SUPABASE_SERVICE_KEY` variable with a Supabase Service role key or Secret keys.
+   > Make sure to set the variable type to secret variable so Cloudflare won't override it when a new version deploys
+5. Hit `Deploy` button once you filled all variables.
+6. Add a custom domain to your Worker for routing.
 
-### Bug reporting
+> Note that, in my code, the `*.workers.dev` domain is disabled because I don't want traffic from other places.
+> 
+> Set `workers_dev` to `true` in `wrangler.jsonc` to enable `*.workers.dev` domain.
 
-Please send a mail to `me at wolf-yuan.dev`
+### Bug reporting / additional station inquiry
+
+Please send a mail to `me@wolf-yuan.dev`
--- apps/grabber/database/init.sql
+++ apps/grabber/database/init.sql
@@ -1,69 +1,109 @@
 CREATE TYPE station_state AS ENUM ('FULL', 'EMPTY', 'NORMAL');
 
+--- Create tables
+CREATE TABLE public.current
+(
+    station_id  integer                  NOT NULL,
+    unavailable integer                  NOT NULL DEFAULT 0,
+    success     integer                  NOT NULL DEFAULT 0,
+    update      timestamp with time zone NOT NULL DEFAULT (now()),
+    bikes       smallint                 NOT NULL DEFAULT 0,
+    slots       smallint                 NOT NULL DEFAULT 0,
+    full        integer                  NOT NULL DEFAULT 0,
+    status USER-DEFINED NOT NULL DEFAULT 'NORMAL'::station_state,
+    CONSTRAINT current_pkey PRIMARY KEY (station_id),
+    CONSTRAINT history_station_id_fkey FOREIGN KEY (station_id) REFERENCES public.stations (id)
+);
+CREATE TABLE public.history
+(
+    id         uuid                     NOT NULL DEFAULT gen_random_uuid(),
+    station_id integer                  NOT NULL,
+    available  smallint                 NOT NULL,
+    empty      smallint                 NOT NULL,
+    at         timestamp with time zone NOT NULL,
+    CONSTRAINT history_pkey PRIMARY KEY (id),
+    CONSTRAINT recently_station_id_fkey FOREIGN KEY (station_id) REFERENCES public.stations (id)
+);
 CREATE TABLE public.stations
 (
-    id      BIGINT           NOT NULL,
-    name    TEXT             NOT NULL,
-    lat     DOUBLE PRECISION NOT NULL,
-    lng     DOUBLE PRECISION NOT NULL,
-    enabled BOOLEAN          NOT NULL,
-    address TEXT             NOT NULL,
-    total   SMALLINT NULL,
+    id         integer  NOT NULL,
+    name       text     NOT NULL,
+    lat        real     NOT NULL,
+    lng        real     NOT NULL,
+    address    text     NOT NULL,
+    total      smallint NOT NULL DEFAULT 0,
+    name_en    text     NOT NULL,
+    address_en text     NOT NULL,
     CONSTRAINT stations_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE public.current
-(
-    station_id  BIGINT                   NOT NULL,
-    unavailable BIGINT                   NOT NULL DEFAULT 0,
-    success     BIGINT                   NOT NULL DEFAULT 0,
-    fail        BIGINT                   NOT NULL DEFAULT 0,
-    update      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'Asia/Taipei'::TEXT),
-    bikes       SMALLINT                 NOT NULL DEFAULT 0,
-    slots       SMALLINT                 NOT NULL DEFAULT 0,
-    full        BIGINT                   NOT NULL DEFAULT 0,
-    status      station_state            NOT NULL DEFAULT 'NORMAL'::station_state,
-    CONSTRAINT history_pkey PRIMARY KEY (station_id),
-    CONSTRAINT history_station_id_fkey FOREIGN KEY (station_id)
-        REFERENCES stations (id) ON DELETE CASCADE
-);
-
-CREATE TABLE public.history
-(
-    id         UUID                     NOT NULL DEFAULT GEN_RANDOM_UUID(),
-    station_id BIGINT                   NOT NULL,
-    available  INTEGER                  NOT NULL,
-    empty      INTEGER                  NOT NULL,
-    at         TIMESTAMP WITH TIME ZONE NOT NULL,
-    CONSTRAINT recently_pkey PRIMARY KEY (id),
-    CONSTRAINT recently_station_id_fkey FOREIGN KEY (station_id)
-        REFERENCES stations (id) ON DELETE CASCADE
-);
-
+--- Create a 5-day history trigger, for no-brain insert from grabber
 CREATE
-OR REPLACE FUNCTION cleanup_recently_after_insert()
-RETURNS TRIGGER AS $$
-
+OR REPLACE FUNCTION public.cleanup_recently_after_insert()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SET search_path TO 'public'
+AS $function$
 BEGIN
-  SET
-search_path = public;
-
 DELETE
 FROM history
 WHERE id IN (SELECT id
              FROM (SELECT id,
-                          ROW_NUMBER() OVER (PARTITION BY station_id ORDER BY at DESC) AS rn
+                          ROW_NUMBER() OVER (
+               PARTITION BY station_id, date_trunc('day', at)
+               ORDER BY date_trunc('day', at) DESC
+             ) AS day_rn, DENSE_RANK() OVER (
+               PARTITION BY station_id
+               ORDER BY date_trunc('day', at) DESC
+             ) AS day_rank
                    FROM history
                    WHERE station_id = NEW.station_id) sub
-             WHERE rn > 48);
+             WHERE day_rank > 5);
 RETURN NEW;
 END;
+$function$
 
-$$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER trg_cleanup_recently_after_insert
+CREATE TRIGGER "trg_cleanup_recently_after_insert"
     AFTER INSERT
-    ON history
+    ON "public"."history"
     FOR EACH ROW
-    EXECUTE FUNCTION cleanup_recently_after_insert();
+    EXECUTE FUNCTION "public"."cleanup_recently_after_insert"();
+
+--- Create get_history_with_limit function used in home page
+CREATE
+OR REPLACE FUNCTION public.get_history_with_limit(max_rows integer DEFAULT 3)
+ RETURNS TABLE(id uuid, station_id integer, available smallint, empty smallint, at timestamp with time zone)
+ LANGUAGE plpgsql
+ STABLE
+ SET search_path TO 'public'
+AS $function$
+BEGIN
+RETURN QUERY WITH ranked_data AS (
+    SELECT r.*,
+           ROW_NUMBER() OVER (PARTITION BY r.station_id ORDER BY r.at DESC) AS row_num
+    FROM history r
+  )
+SELECT r.id, r.station_id, r.available, r.empty, r.at
+FROM ranked_data r
+WHERE r.row_num <= 6
+ORDER BY r.at DESC;
+END;
+$function$
+
+--- Configure RLS
+alter
+policy "Enable read access for all users"
+on "public"."current"
+to public
+using (true);
+      alter
+
+policy "Enable read access for all users"
+on "public"."history"
+to public
+using (true);
+            alter
+policy "Enable read access for all users"
+on "public"."stations"
+to public
+using (true);
