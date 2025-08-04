dbee7f1890b89678f38f8817ecb09ec1f3b647a2
wolf-yuan <wolf@wolf-yuan.dev>
2025-08-04 23:53:04 +0800

    refactor: migrate to old supabase client

--- apps/frontend/src/lib/supabase.ts
+++ apps/frontend/src/lib/supabase.ts
@@ -1,14 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@typing/database.types";
 
-interface Env {
-  SUPABASE_URL: string;
-  SUPABASE_PUBLISHABLE: string;
-}
-
-export function createSupabaseClient(env: Env) {
-  return createClient<Database>(
-    env.SUPABASE_URL || import.meta.env.SUPABASE_URL,
-    env.SUPABASE_PUBLISHABLE || import.meta.env.SUPABASE_PUBLISHABLE,
-  );
-}
+export const supabase = createClient<Database>(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_PUBLISHABLE,
+);
