27bc62d976d60150ba17131c1f89a6e2f13b0d57
wolf-yuan <wolf@wolf-yuan.dev>
2025-10-18 17:47:49 +0800

    fix: Bun is not defined on Node runtime

--- apps/api/src/lib/supabase.ts
+++ apps/api/src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../typing/database.types";
 
 export const supabase = createClient<Database>(
-  Bun.env.SUPABASE_URL || process.env.SUPABASE_URL || "",
-  Bun.env.SUPABASE_PUBLISHABLE || process.env.SUPABASE_PUBLISHABLE || "",
+  Bun?.env.SUPABASE_URL || process.env.SUPABASE_URL || "",
+  Bun?.env.SUPABASE_PUBLISHABLE || process.env.SUPABASE_PUBLISHABLE || "",
 );
