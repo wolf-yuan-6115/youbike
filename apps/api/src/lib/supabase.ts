df171de167bcf41c0ac9f6314352a85be2cc77b3
wolf-yuan <wolf@wolf-yuan.dev>
2025-10-18 17:51:02 +0800

    fix: some weird Bun thing

--- apps/api/src/lib/supabase.ts
+++ apps/api/src/lib/supabase.ts
@@ -1,7 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../typing/database.types";
 
+let SUPABASE_URL = Bun ? Bun.env.SUPABASE_URL : process.env.SUPABASE_URL;
+let SUPABASE_PUBLISHABLE = Bun
+  ? Bun.env.SUPABASE_PUBLISHABLE
+  : process.env.SUPABASE_PUBLISHABLE;
+
 export const supabase = createClient<Database>(
-  Bun?.env.SUPABASE_URL || process.env.SUPABASE_URL || "",
-  Bun?.env.SUPABASE_PUBLISHABLE || process.env.SUPABASE_PUBLISHABLE || "",
+  SUPABASE_URL!,
+  SUPABASE_PUBLISHABLE!,
 );
