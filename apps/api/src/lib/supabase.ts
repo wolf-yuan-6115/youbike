4eebf76aebc96cf6a09f34d8c340312aac2e5396
wolf-yuan <wolf@wolf-yuan.dev>
2025-10-18 17:53:51 +0800

    fix: bun is not defined in node

--- apps/api/src/lib/supabase.ts
+++ apps/api/src/lib/supabase.ts
@@ -1,8 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../typing/database.types";
 
-let SUPABASE_URL = Bun ? Bun.env.SUPABASE_URL : process.env.SUPABASE_URL;
-let SUPABASE_PUBLISHABLE = Bun
+const isRunningOnBun = typeof Bun !== "undefined";
+
+const SUPABASE_URL = isRunningOnBun
+  ? Bun.env.SUPABASE_URL
+  : process.env.SUPABASE_URL;
+const SUPABASE_PUBLISHABLE = isRunningOnBun
   ? Bun.env.SUPABASE_PUBLISHABLE
   : process.env.SUPABASE_PUBLISHABLE;
 
